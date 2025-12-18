14b3f16118d3cfa44cc602b44d26095efd0f8ca3
Barshat Pradhan <barshatpradhan321@gmail.com>
2025-12-18 18:48:38 -0500

    edit destination page

--- src/components/desktop/DestinationCard.jsx
+++ src/components/desktop/DestinationCard.jsx
@@ -1,60 +1,7 @@
-// import React from 'react';
-// // import heartIcon from '/src/assets/Heart-icon.png';
-// // import locationIcon from '/src/assets/location.png';
-
-// export default function DestinationCard({
-//   title = 'Darjeeling',
-//   location = 'West Bengal, India',
-//   img = '',
-//   favorite = false,
-//   onFavorite = () => {}
-// }) {
-//   return (
-//     <article className="dest-card card" role="article" aria-label={title}>
-
-//       {/* IMAGE */}
-//       <div className="dest-media">
-//         <img src={img} alt={title} />
-
-//         {/* HEART ICON */}
-//         <button className="dest-fav" onClick={onFavorite}>
-//           ♥
-//         </button>
-//       </div>
-
-//       {/* BODY */}
-//       <div className="dest-body">
-//         <div>
-//           <div className="dest-title">{title}</div>
-
-//           <div className="dest-location">
-//             <img src="/src/assets/location.png" alt="" />
-//             {location}
-//           </div>
-//         </div>
-
-//         {/* ACTION */}
-//         <div className="dest-action">
-//           {/* OVERLAPPING CIRCLES */}
-//           <div className="dest-circles">
-//             <span />
-//             <span />
-//             <span />
-//             <span />
-//           </div>
-
-//           <button className="btn-book">Book Now</button>
-//         </div>
-//       </div>
-//     </article>
-//   );
-// }
-
-
 
 
 import heartIcon from '../../assets/Heart-icon.png';
-import heartFilledIcon from '../../assets/Heart-icon.png';
+// import heartFilledIcon from '../../assets/Heart-icon.png';
 import locationIcon from '../../assets/location.png';
 
 export default function DestinationCard({
@@ -75,7 +22,7 @@ export default function DestinationCard({
 
         <button className="dest-fav" onClick={onFavorite}>
           <img
-            src={favorite ? heartFilledIcon : heartIcon}
+            src={favorite ? heartIcon : heartIcon}
             alt="Favorite"
           />
         </button>
